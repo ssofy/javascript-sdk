1693fa91e078d89f2ef8946977a99e8e8d7573ad
SSOfy <support@ssofy.com>
2023-08-05 12:36:33 +0300

    fixed tests

--- src/__test__/OAuth2Client/OAuth2Client.test.ts
+++ src/__test__/OAuth2Client/OAuth2Client.test.ts
@@ -2,26 +2,29 @@ import {OAuth2Config} from "../../OAuth2Config";
 import {OAuth2Client} from "../../OAuth2Client";
 import {Storage} from "../../Storage/Storage";
 import {UrlHelper} from "../../Helpers/UrlHelper";
-import {MemoryStorage} from "../../Storage/MemoryStorage";
+import {FileStorage} from "../../Storage/FileStorage";
 
-describe('Client Test', () => {
-    const stateStore = new MemoryStorage();
+describe('OAuth2Client Test', () => {
+    const storagePath = '/tmp/';
+    const stateStore = new FileStorage(storagePath);
 
     const config = new OAuth2Config({
         url: 'http://sandbox.us.ssofy.local',
         clientId: 'sandbox',
         clientSecret: 'sandbox',
         scopes: ['*'],
+        pkceVerification: true,
         redirectUri: 'https://oauthdebugger.com/debug',
         stateStore: <Storage>stateStore,
     });
 
     const client = new OAuth2Client(config);
 
-    test.skip('auth code flow initiation', async () => {
-        jest.setTimeout(30000);
+    jest.setTimeout(5000);
 
-        const state = await client.initAuthCodeFlow('http://localhost/oauth/continue');
+    test.skip('auth code flow initiation', async () => {
+
+        const state = await client.initAuthCodeFlow();
         console.log(state);
 
         expect(state.state).toBeDefined();
@@ -29,9 +32,7 @@ describe('Client Test', () => {
     });
 
     test.skip('implicit flow initiation', async () => {
-        jest.setTimeout(30000);
-
-        const state = await client.initImplicitFlow('http://localhost/oauth/continue');
+        const state = await client.initImplicitFlow();
         console.log(state);
 
         expect(state.state).toBeDefined();
@@ -39,37 +40,45 @@ describe('Client Test', () => {
     });
 
     test.skip('callback handle', async () => {
-        jest.setTimeout(30000);
+        const state = 'tnhDYP9R5X';
+        const code = '0189c4f6b3c17e4d97f85721961359a6955bd7cc8cd74b80b11cf4a06af4ee81513e8094a09e402193e37bc790015bca';
 
-        const state = '9zSTfBO3p1';
-
-        const payload = UrlHelper.getParameters(`https://oauthdebugger.com/debug?code=018998dcddc578668890b15277010ea7469b2cd11e9a45fc9b30167c6e6754d0698987de47324da2a400c90c0a9782e2&state=${state}`);
+        const payload = UrlHelper.getParameters(`https://oauthdebugger.com/debug?code=${code}&state=${state}`);
         console.log(payload);
 
-        const stateData = await client.handleCallback(state, payload);
+        const stateData = await client.handleCallback(payload);
         console.log(stateData.token);
 
         expect(true).toBeTruthy();
     });
 
-    test.skip('refresh token', async () => {
-        jest.setTimeout(30000);
+    test.skip('access token', async () => {
+        const state = 'tnhDYP9R5X';
 
-        const state = await client.getAccessToken('9zSTfBO3p1');
-        console.log(state);
+        const stateData = await client.getAccessToken(state);
+        console.log(stateData);
+
+        expect(true).toBeTruthy();
+    });
+
+    test.skip('renew token', async () => {
+        const state = 'tnhDYP9R5X';
+
+        const stateData = await client.renewAccessToken(state);
+        console.log(stateData);
 
         expect(true).toBeTruthy();
     });
 
     test.skip('refresh user info', async () => {
-        jest.setTimeout(30000);
-
         let user;
 
-        user = await client.getUserInfo('9zSTfBO3p1');
+        const state = 'tnhDYP9R5X';
+
+        user = await client.getUserInfo(state);
         console.log(user);
 
-        user = await client.refreshUserInfo('9zSTfBO3p1');
+        user = await client.refreshUserInfo(state);
         console.log(user);
 
         expect(true).toBeTruthy();
