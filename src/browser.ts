0220946239219154333f375adf6c3e7bb4b0c33f
SSOfy <support@ssofy.com>
2024-01-27 17:27:08 +0300

    added SessionStorage

--- src/browser.ts
+++ src/browser.ts
@@ -5,5 +5,6 @@ export {Storage} from "./Storage/Storage";
 export {NullStorage} from "./Storage/NullStorage";
 export {MemoryStorage} from "./Storage/MemoryStorage";
 export {LocalStorage} from "./Storage/LocalStorage";
+export {SessionStorage} from "./Storage/SessionStorage";
 export {CookieStorage} from "./Storage/CookieStorage";
 export {UrlHelper} from "./Helpers/UrlHelper";
